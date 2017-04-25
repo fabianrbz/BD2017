ca0c8dea6d528f22577b3e22579094b2de91293d
Fabian Rodriguez <fabianrbz@gmail.com>
2017-04-25 07:17:32 +0200

    Package everything into a maven project.

---
+++ labs/1-mapreduce-hadoop/maven/WordCount/src/main/java/fabianrbz/WordCount.java
@@ -0,0 +1,66 @@
+package fabianrbz;
+import java.io.IOException;
+import java.util.*;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.conf.*;
+import org.apache.hadoop.io.*;
+import org.apache.hadoop.mapreduce.*;
+import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
+import org.apache.hadoop.mapreduce.lib.input.TextInputFormat;
+import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
+import org.apache.hadoop.mapreduce.lib.output.TextOutputFormat;
+
+public class WordCount {
+
+  public static class Map extends Mapper<LongWritable, Text, Text, IntWritable> {
+
+    private static final IntWritable one = new IntWritable(1);
+    private Text word = new Text();
+
+    public void map(LongWritable key, Text value, Context context) throws IOException, InterruptedException {
+
+      String line = value.toString();
+      StringTokenizer tokens = new StringTokenizer(line, " \t\n\r\f,.:;?![]'");
+
+      while (tokens.hasMoreTokens()) {
+        word.set(tokens.nextToken().toLowerCase());
+        context.write(word, one);
+      }
+
+    }
+  }
+
+  public static class Reduce extends Reducer<Text, IntWritable, Text, IntWritable> {
+
+    public void reduce(Text key, Iterable<IntWritable> values, Context context) throws IOException, InterruptedException {
+
+      int sum = 0;
+      for (IntWritable val : values) {
+        sum += val.get();
+      }
+      context.write(key, new IntWritable(sum));
+
+    }
+  }
+
+  public static void main(String[] args) throws Exception {
+    Configuration conf = new Configuration();
+
+    Job job = new Job(conf, "wordcount");
+
+    job.setOutputKeyClass(Text.class);
+    job.setOutputValueClass(IntWritable.class);
+
+    job.setMapperClass(Map.class);
+    job.setReducerClass(Reduce.class);
+
+    job.setInputFormatClass(TextInputFormat.class);
+    job.setOutputFormatClass(TextOutputFormat.class);
+
+    FileInputFormat.addInputPath(job, new Path(args[0]));
+    FileOutputFormat.setOutputPath(job, new Path(args[1]));
+
+    job.waitForCompletion(true);
+  }
+
+}
